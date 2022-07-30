9455a9b2773c58d58fbf1e83a3f81a652c33b8cc
Merite15 <86039889+Merite15@users.noreply.github.com>
2022-07-30 09:19:50 +0200

    finish test

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -8,7 +8,7 @@ class BookController extends Controller
 {
     public function store(Request $req)
     {
-        auth()->user()->books()->create(['title', 'author', 'published_year', 'description', 'price', 'user_id']);
+        auth()->user()->books()->create($req->only(['title', 'author', 'published_year', 'description', 'price', 'user_id']));
 
         return response()->json(['message' => 'Book created']);
     }
